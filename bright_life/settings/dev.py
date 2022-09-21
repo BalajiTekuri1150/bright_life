8c54da793e2b927038a860cf6fb4f1cf83a55399
BalajiTekuri1150 <tekuribalaji1150@gmail.com>
2022-09-21 19:18:59 +0530

    commented staticfile_dirs in dev settings

--- bright_life/settings/dev.py
+++ bright_life/settings/dev.py
@@ -64,8 +64,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-STATICFILES_DIRS = [
-os.path.join(BASE_DIR,'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 # STATICFILES_DIRS = [
 # os.path.join(BASE_DIR,'static')]   # This is your static folder
