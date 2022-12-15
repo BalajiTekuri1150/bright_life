bc6a10d4dbc64657478cc6c35f8150ca65ab7646
BalajiTekuri1150 <tekuribalaji1150@gmail.com>
2022-12-15 10:03:47 +0530

    added is_active in update guardian profile api

--- bright_life_app/views.py
+++ bright_life_app/views.py
@@ -760,6 +760,7 @@ class UpdateGuardianProfile(APIView):
                     instance = Guardian.objects.get(id = id)
                     data['created_by'] = instance.created_by
                     data['last_updated_by'] = request.user.name
+                    data['is_active'] = True
                     userObj = data.pop('user')
                     logger.info("user object :"+str(userObj))
                     user = json.loads(userObj[0])
