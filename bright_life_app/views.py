56629a447234b9d14449a54de9592fbeb6f91c55
BalajiTekuri1150 <tekuribalaji1150@gmail.com>
2023-06-20 11:43:06 +0530

    change in authentication backend for google authentication

--- bright_life_app/views.py
+++ bright_life_app/views.py
@@ -208,7 +208,7 @@ class GoogleSignIn(APIView):
                     user = User.objects.get(email=email)
                     logger.info("user :"+str(user))
                     # Authenticate and log in the user
-                    user = authenticate(request, email=email)
+                    user = authenticate(request, username=email, password=None, backend='allauth.account.auth_backends.AuthenticationBackend')
                     login(request, user)
 
                     # Generate a token for the authenticated user
